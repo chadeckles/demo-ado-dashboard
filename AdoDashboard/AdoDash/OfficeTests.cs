ffc62b8fd4068c20fc31ac15ff10f2f3d5faea7d
nadvolod <nadvolod@gmail.com>
2020-01-30 10:41:33 -0800

    add a performance test

--- AdoDashboard/AdoDash/OfficeTests.cs
+++ AdoDashboard/AdoDash/OfficeTests.cs
@@ -15,12 +15,27 @@ namespace AdoDash
     {
         private Dictionary<string, object> sauceOptions;
         private IWebDriver _driver;
+        private ChromeOptions chromeOptions;
+
         public TestContext TestContext { get; set; }
+        public string sauceUserName { get; private set; }
+        public string sauceAccessKey { get; private set; }
 
         [TestMethod]
         public void ShouldOpen()
         {
-            _driver = GetDriver();
+            var sauceOptions = new Dictionary<string, object>
+            {
+                ["username"] = sauceUserName,
+                ["accessKey"] = sauceAccessKey,
+                ["name"] = TestContext.TestName,
+                ["extendedDebugging"] = true,
+                ["capturePerformance"] = true
+            };
+            chromeOptions.AddAdditionalOption("sauce:options", sauceOptions);
+            _driver = new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
+                chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(600));
+
             _driver.Navigate().GoToUrl("https://www.office.com/?auth=1");
 
             var user = Environment.GetEnvironmentVariable("365_USER", 
@@ -45,18 +60,37 @@ namespace AdoDash
                 ExpectedConditions.ElementIsVisible(By.Id("idSIButton9")));
             button.Click();
         }
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            var sauceOptions = new Dictionary<string, object>
+            {
+                ["username"] = sauceUserName,
+                ["accessKey"] = sauceAccessKey,
+                ["name"] = TestContext.TestName,
+                ["extendedDebugging"] = true,
+                ["capturePerformance"] = true
+            };
+            chromeOptions.AddAdditionalOption("sauce:options", sauceOptions);
+            _driver = new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
+                chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(600));
+
+            _driver.Navigate().GoToUrl("https://www.office.com/?auth=1");
+        }
         [TestInitialize]
         public void SetupTests()
         {
             //TODO please supply your Sauce Labs user name in an environment variable
-            var sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
             //TODO please supply your own Sauce Labs access Key in an environment variable
-            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
-            sauceOptions = new Dictionary<string, object>
+            sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+            chromeOptions = new ChromeOptions()
             {
-                ["username"] = sauceUserName,
-                ["accessKey"] = sauceAccessKey
+                BrowserVersion = "latest",
+                PlatformName = "Windows 10",
+                UseSpecCompliantProtocol = true
             };
+
         }
         [TestCleanup]
         public void CleanUpAfterEveryTestMethod()
@@ -65,20 +99,6 @@ namespace AdoDash
             if (_driver is null) return;
             ((IJavaScriptExecutor) _driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
             _driver?.Quit();
-        }
-
-        private IWebDriver GetDriver()
-        {
-            var chromeOptions = new ChromeOptions
-            {
-                BrowserVersion = "latest",
-                PlatformName = "Windows 10"
-            };
-            sauceOptions.Add("name", MethodBase.GetCurrentMethod().Name);
-            chromeOptions.AddAdditionalOption("sauce:options", sauceOptions);
-
-            return new RemoteWebDriver(new Uri("https://ondemand.saucelabs.com/wd/hub"),
-                chromeOptions.ToCapabilities(), TimeSpan.FromSeconds(60));
         }
     }
 }
