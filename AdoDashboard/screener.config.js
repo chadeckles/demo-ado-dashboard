7715ab89fcfec3ae77a31159268725980a5eb2aa
nadvolod <nadvolod@gmail.com>
2020-02-03 11:54:50 -0500

    add node.js project to the solution for visual testing

---
+++ AdoDashboard/screener.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // full repository name for your project:
+    projectRepo: 'testVisual',
+
+    // this example assumes Environment Variables listed below exist on your system:
+    apiKey: process.env.SCREENER_API_KEY,
+
+    // array of UI states to capture visual snapshots of.
+    // each state consists of a url and a name.
+    states: [
+        {
+            url: 'http://www.google.com',
+            name: 'Name of UI State'
+        }
+    ]
+};
