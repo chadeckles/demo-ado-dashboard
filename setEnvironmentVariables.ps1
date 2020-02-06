a61e812267b25b0c2db0811b8e6c8dad336a9aa0
nadvolod <nadvolod@gmail.com>
2020-02-06 14:47:34 -0500

    add 365 variables

--- setEnvironmentVariables.ps1
+++ setEnvironmentVariables.ps1
@@ -1,17 +1,15 @@
 Param(
 [string]$sauceUserName,
 [string]$sauceAccessKey,
-[string]$rdcVodQaNativeAppApiKey,
-[string]$rdcSauceDemoIosRdcApiKey
+[string]$office365UserName,
+[string]$office365Password
 )
-Write-Output "sauce.userName value from ADO was passed as an Argument in the ADO Task called `$env:SAUCE_USERNAME`
-to sauceUserName variable in the Posh. This is the value found=>$sauceUserName"
-Write-Output "sauce.accessKey value from ADO was passed as an Argument in the ADO Task called `$env:SAUCE_ACCESS_KEY`
-to sauceAccessKey variable in the Posh. This is the value found=>$sauceAccessKey"
-Write-Output "sauce.rdc.VodQaNativeAppApiKey stored in Azure DevOps=>$rdcVodQaNativeAppApiKey"
-Write-Output "sauce.rdc.SauceDemoIosRdcApiKey stored in Azure DevOps=>$rdcSauceDemoIosRdcApiKey"
+Write-Output "sauce.userName stored in Azure DevOps=>=>$sauceUserName"
+Write-Output "sauce.accessKey stored in Azure DevOps=>=>$sauceAccessKey"
+Write-Output "365.user stored in Azure DevOps=>$rdcVodQaNativeAppApiKey"
+Write-Output "365.pass stored in Azure DevOps=>$rdcSauceDemoIosRdcApiKey"
 
 [Environment]::SetEnvironmentVariable("SAUCE_USERNAME", "$sauceUserName", "User")
 [Environment]::SetEnvironmentVariable("SAUCE_ACCESS_KEY", "$sauceAccessKey", "User")
-[Environment]::SetEnvironmentVariable("VODQC_RDC_API_KEY", "$rdcVodQaNativeAppApiKey", "User")
-[Environment]::SetEnvironmentVariable("SAUCE_DEMO_IOS_RDC_API_KEY", "$rdcSauceDemoIosRdcApiKey", "User")
+[Environment]::SetEnvironmentVariable("365_USER", "$office365UserName", "User")
+[Environment]::SetEnvironmentVariable("365_PASS", "$office365Password", "User")
